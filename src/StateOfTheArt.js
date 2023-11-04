8acbab13159bba4ac5be2987145149f955ed3b15
anisbc <bachaanis0000@gmail.com>
2023-11-04 13:48:44 +0100

    alt attribute added

--- src/StateOfTheArt.js
+++ src/StateOfTheArt.js
@@ -1,7 +1,7 @@
 const StateOfTheArt = () => {
     return (
         <article className="main__illustration">
-            <img src={require('./images/illustration-phones.svg').default}/>
+            <img src={require('./images/illustration-phones.svg').default} alt='phones'/>
             <section className='main__state-of-art'>
                 <h2>State of the Art Infrastructure</h2>
                 <p>With reliablity and speed in mind, worldwide data centers provide teh backbone for ultra-fast connectivity. This ensures your site will load instantly no matter where your readers are, keeping your site competitive.</p>
